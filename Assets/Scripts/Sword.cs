2be981d074320e5fa6997c0c9f68263281b1ceb2
Andrzej Skwierawski <skwierawski.andrzej@gmail.com>
2019-12-02 14:54:27 +0100

    Sword added to scene. Also movement of sword with mouse and rotation with keys.

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sword : MonoBehaviour
+{
+	public Rigidbody RB;
+
+	public bool IsActive = false;
+	public float radius;
+	public float hight;
+
+	public float SwordMoveSpeed   = 2f;
+	public float SwordRotateSpeed = 25f;
+
+	private float moveSpeed;
+	private float rotateSpeed;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		moveSpeed   = SwordMoveSpeed;
+		rotateSpeed = SwordRotateSpeed;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(moveSpeed * Input.GetAxis("Mouse X") * Time.deltaTime, moveSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime, 0f);
+		transform.Rotate(rotateSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0f, -rotateSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+    }
+}
