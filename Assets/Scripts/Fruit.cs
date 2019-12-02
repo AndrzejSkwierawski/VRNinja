349da834996e378331e87c774b548b851b496d7e
Misiek95 <michal.wozniak16@gmail.com>
2019-12-02 22:32:04 +0100

    Generate and moving balls

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject przeciwnik;
+
+    private float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer>5)
+        {
+            Generate();
+            timer = 0;
+        }
+    }
+
+    void Generate()
+    {
+        Vector3 spawnFruit = new Vector3(Random.Range(-3f, 3f), 1f, 8f);
+        Instantiate(przeciwnik, spawnFruit, Quaternion.identity);
+    }
+
+
+
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    private Vector3 finalPoint;
+    private float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalPoint = new Vector3(Random.Range(-1f, 1f), 0f,-1f);
+        moveSpeed = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = finalPoint - transform.position;
+        transform.Translate(moveSpeed * direction[0] * Time.deltaTime, 
+            moveSpeed * direction[1] * Time.deltaTime, moveSpeed * direction[2] * Time.deltaTime);
+       
+    }
+}
